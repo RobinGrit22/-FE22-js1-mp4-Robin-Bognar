fd07d35b734f50a95fd2fa2c92cbaecfd9ff5067
RobinGrit22 <112506404+RobinGrit22@users.noreply.github.com>
2022-12-12 21:22:43 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+
+const animation= {
+    targets: '.animeBoxes',
+    translateX: [
+        {value: 250, duration: 500},
+    ],
+    rotate: '1turn',
+    easing: 'easeInOutQuad',
+    delay: anime.stagger(1000),
+    endDelay: 200,
+    direction: 'alternate',
+    backgroundColor: '#FF33DA',
+    borderRadius: ['0%', '50%'],
+    opacity: '0.5',
+    autoplay: false
+
+}
+
+const help = anime(animation);
+
+
+const playBtn = document.querySelector("#play");
+const pauseBtn = document.querySelector("#pause");
+const stopBtn = document.querySelector("#stop");
+
+playBtn.addEventListener("click", help.play);
+pauseBtn.addEventListener("click", help.pause);
